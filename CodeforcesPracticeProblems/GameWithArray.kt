6bedab3eaaf827805d4b80cb6163a251cabccdad
AMAN PATEL <ap289503@gmail.com>
2021-07-10 02:24:15 +0530

    added GameWithArray(CF) into practice folder

---
+++ CodeforcesPracticeProblems/GameWithArray.kt
@@ -0,0 +1,71 @@
+import java.io.PrintWriter
+import java.util.StringTokenizer
+import java.util.*
+import java.io.File
+
+private val checkOnlineJudge = System.getProperty("ONLINE_JUDGE") == null
+private val INPUT = if (checkOnlineJudge) File("src/input.txt").inputStream() else System.`in`
+private val OUTPUT = if (checkOnlineJudge) File("src/output.txt").outputStream() else System.out
+
+private val bufferedReader = INPUT.bufferedReader()
+private val outputWriter = PrintWriter(OUTPUT, false)
+private fun readLn() = bufferedReader.readLine()!!
+
+private fun readList() = readLn().split(' ')
+private var tokenizer = StringTokenizer("")
+private fun read(): String {
+    while (tokenizer.hasMoreTokens().not()) tokenizer = StringTokenizer(readLn(), " ")
+    return tokenizer.nextToken()
+}
+
+private fun readInt() = read().toInt()
+private fun readLong() = read().toLong()
+private fun readDouble() = read().toDouble()
+
+private fun readIntList() = readList().map { it.toInt() }
+private fun readLongList() = readList().map { it.toLong() }
+private fun readDoubleList() = readList().map { it.toDouble() }
+
+private fun readIntArray(n: Int = 0) =
+    if (n == 0) readList().run { IntArray(size) { get(it).toInt() } } else IntArray(n) { readInt() }
+
+private fun readLongArray(n: Int = 0) =
+    if (n == 0) readList().run { LongArray(size) { get(it).toLong() } } else LongArray(n) { readLong() }
+
+private fun readDoubleArray(n: Int = 0) =
+    if (n == 0) readList().run { DoubleArray(size) { get(it).toDouble() } } else DoubleArray(n) { readDouble() }
+
+private class GameWithArray {
+    fun solveTestCase() {
+        //TODO: Solve the question
+
+        val (n, s) = readIntList()
+
+        val check = s / n;
+        if (check >= 2) {
+            val left = s - ((n - 1) * 2)
+            outputWriter.println("YES")
+            for (i in 0 until (n - 1))
+                outputWriter.print("2 ")
+            outputWriter.println(left)
+            outputWriter.print(1)
+        } else
+            outputWriter.println("NO")
+
+        return
+    }
+}
+
+fun main(args: Array<String>) {
+
+    var t = 1
+    //t = readInt()
+    repeat(t) {
+        //TODO: Read in each Test Case
+
+        GameWithArray()
+            .solveTestCase()
+    }
+
+    outputWriter.flush()
+}
