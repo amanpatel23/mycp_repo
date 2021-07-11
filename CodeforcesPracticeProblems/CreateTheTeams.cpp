3cbc846ea03198ff0e008048b5892e60c8a8f010
AMAN PATEL <ap289503@gmail.com>
2021-07-11 15:14:07 +0530

    added CreateTheTeams(CF) into Practice folder

---
+++ CodeforcesPracticeProblems/CreateTheTeams.cpp
@@ -0,0 +1,79 @@
+import java.io.PrintWriter
+import java.util.StringTokenizer
+import java.util.*
+import java.io.File
+
+private val checkOnlineJudge = System.getProperty("ONLINE_JUDGE") == null
+private val INPUT = if (checkOnlineJudge) File("src/input.txt").inputStream() else System.`in`
+private val OUTPUT = if (checkOnlineJudge) File("src/output.txt").outputStream() else System.out
+
+private val bufferedReader = INPUT.bufferedReader()
+private val outputWriter = PrintWriter(OUTPUT, false)
+private fun readLn() = bufferedReader.readLine()!!
+
+private fun readList() = readLn().split(' ')
+private var tokenizer = StringTokenizer("")
+private fun read(): String {
+    while (tokenizer.hasMoreTokens().not()) tokenizer = StringTokenizer(readLn(), " ")
+    return tokenizer.nextToken()
+}
+
+private fun readInt() = read().toInt()
+private fun readLong() = read().toLong()
+private fun readDouble() = read().toDouble()
+
+private fun readIntList() = readList().map { it.toInt() }
+private fun readLongList() = readList().map { it.toLong() }
+private fun readDoubleList() = readList().map { it.toDouble() }
+
+private fun readIntArray(n: Int = 0) =
+    if (n == 0) readList().run { IntArray(size) { get(it).toInt() } } else IntArray(n) { readInt() }
+
+private fun readLongArray(n: Int = 0) =
+    if (n == 0) readList().run { LongArray(size) { get(it).toLong() } } else LongArray(n) { readLong() }
+
+private fun readDoubleArray(n: Int = 0) =
+    if (n == 0) readList().run { DoubleArray(size) { get(it).toDouble() } } else DoubleArray(n) { readDouble() }
+
+private class CreateTheTeams {
+    fun solveTestCase(): Int {
+        //TODO: Solve the question
+
+        val n = readInt()
+        var x = readLong()
+        val skills = mutableListOf<Long>()
+        for (i in 0 until n)
+            skills.add(readLong())
+
+        skills.sort()
+        var result = 0
+        var ele = 0
+        for (i in (n - 1) downTo 0) {
+            ele++
+            val lhs: Long = ele.toLong() * skills[i]
+            if (lhs >= x) {
+                result++
+                ele = 0
+            }
+        }
+
+
+        return result
+    }
+}
+
+fun main(args: Array<String>) {
+
+    var t = 1
+    t = readInt()
+    repeat(t) {
+        //TODO: Read in each Test Case
+
+        outputWriter.println(
+            CreateTheTeams()
+                .solveTestCase()
+        )
+    }
+
+    outputWriter.flush()
+}
