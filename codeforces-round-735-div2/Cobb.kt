cf71bbf90231fbb2f1b9c42c0754e7c360ef36ab
AMAN PATEL <ap289503@gmail.com>
2021-07-30 16:48:04 +0530

    added codeforces-round-735-div2

---
+++ codeforces-round-735-div2/Cobb.kt
@@ -0,0 +1,75 @@
+/* 
+    Author: Aman Patel
+    Date: 29-07-2021
+*/
+
+import java.io.PrintWriter
+import java.util.StringTokenizer
+import java.io.File
+
+private val checkOnlineJudge = System.getProperty("ONLINE_JUDGE") == null
+private val INPUT = if (checkOnlineJudge) File("src/input.txt").inputStream() else System.`in`
+private val OUTPUT = if (checkOnlineJudge) File("src/output.txt").outputStream() else System.out
+
+private val bufferedReader = INPUT.bufferedReader()
+private val out = PrintWriter(OUTPUT, false)
+private fun readLn() = bufferedReader.readLine()!!
+
+private fun readList() = readLn().split(' ')
+private var tokenizer = StringTokenizer("")
+private fun read(): String {
+    while (tokenizer.hasMoreTokens().not()) tokenizer = StringTokenizer(readLn(), " ")
+    return tokenizer.nextToken()
+}
+
+private fun readInt() = read().toInt()
+private fun readLong() = read().toLong()
+private fun readDouble() = read().toDouble()
+
+private fun readIntList() = readList().map { it.toInt() }
+private fun readLongList() = readList().map { it.toLong() }
+private fun readDoubleList() = readList().map { it.toDouble() }
+
+private fun readIntArray(n: Int = 0) =
+    if (n == 0) readList().run { IntArray(size) { get(it).toInt() } } else IntArray(n) { readInt() }
+
+private fun readLongArray(n: Int = 0) =
+    if (n == 0) readList().run { LongArray(size) { get(it).toLong() } } else LongArray(n) { readLong() }
+
+private fun readDoubleArray(n: Int = 0) =
+    if (n == 0) readList().run { DoubleArray(size) { get(it).toDouble() } } else DoubleArray(n) { readDouble() }
+
+private class Round735 {
+    fun solveTestCase() {
+        //TODO: Solve the question
+
+        val (n, k) = readIntList()
+        val nums = readLongArray(n)
+
+        val limit = n - 250
+        var result = (-1e15).toLong()
+        for (i in (n - 1) downTo maxOf(limit, 0)) {
+            for (j in (i - 1) downTo maxOf(limit, 0)) {
+                val currResult = ((i + 1) * 1L * (j + 1)) - (k * 1L * (nums[i] or nums[j]))
+                if (currResult > result)
+                    result = currResult
+            }
+        }
+
+        out.println(result)
+    }
+}
+
+fun main(args: Array<String>) {
+
+    var t = 1
+    t = readInt()
+    repeat(t) {
+        //TODO: Read in each Test Case
+
+        Round735()
+            .solveTestCase()
+    }
+
+    out.flush()
+}
