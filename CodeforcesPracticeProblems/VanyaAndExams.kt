0be4e1d247621c7334f913b99bcf16481dc50069
AMAN PATEL <ap289503@gmail.com>
2021-08-13 00:29:10 +0530

    added VanyaAndExams into Practice folder

---
+++ CodeforcesPracticeProblems/VanyaAndExams.kt
@@ -0,0 +1,88 @@
+/* 
+    Author: Aman Patel
+    Date: 13-08-2021
+*/
+
+import java.io.PrintWriter
+import java.util.StringTokenizer
+import java.io.File
+
+private val checkOnlineJudge = System.getProperty("ONLINE_JUDGE") == null
+private val INPUT = if (checkOnlineJudge) File("src/input.txt").inputStream() else System.`in`
+private val OUTPUT = if (checkOnlineJudge) File("src/output.txt").outputStream() else System.out
+
+private val bufferedReader = INPUT.bufferedReader()
+private val out = PrintWriter(OUTPUT, false)
+private fun readLn() = bufferedReader.readLine()!!
+
+private fun readList() = readLn().split(' ')
+private var tokenizer = StringTokenizer("")
+private fun read(): String {
+    while (tokenizer.hasMoreTokens().not()) tokenizer = StringTokenizer(readLn(), " ")
+    return tokenizer.nextToken()
+}
+
+private fun readInt() = read().toInt()
+private fun readLong() = read().toLong()
+private fun readDouble() = read().toDouble()
+
+private fun readIntList() = readList().map { it.toInt() }
+private fun readLongList() = readList().map { it.toLong() }
+private fun readDoubleList() = readList().map { it.toDouble() }
+
+private fun readIntArray(n: Int = 0) =
+    if (n == 0) readList().run { IntArray(size) { get(it).toInt() } } else IntArray(n) { readInt() }
+
+private fun readLongArray(n: Int = 0) =
+    if (n == 0) readList().run { LongArray(size) { get(it).toLong() } } else LongArray(n) { readLong() }
+
+private fun readDoubleArray(n: Int = 0) =
+    if (n == 0) readList().run { DoubleArray(size) { get(it).toDouble() } } else DoubleArray(n) { readDouble() }
+
+private class VanyaAndExams {
+    fun solveTestCase() {
+        //TODO: Solve the question
+
+        val (n, r, avg) = readIntList()
+        val gradeAndEssays = mutableListOf<Pair<Int, Int>>()
+        var sum = 0L
+        for (i in 0 until n) {
+            val (a, b) = readIntList()
+            sum += a
+            gradeAndEssays.add(Pair(a, b))
+        }
+
+        gradeAndEssays.sortBy { it.second }
+        var x = (avg * 1L * n) - sum
+
+        var minEssays = 0L
+        var idx = 0
+        while (true) {
+            if (idx >= n || x <= 0L)
+                break
+
+            val reqGrade = minOf((r - gradeAndEssays[idx].first + 0L), x)
+            val reqEssays = reqGrade * 1L * gradeAndEssays[idx].second
+
+            x -= reqGrade
+            minEssays += reqEssays
+            idx++
+        }
+
+        out.println(minEssays)
+    }
+}
+
+fun main(args: Array<String>) {
+
+    var t = 1
+    //t = readInt()
+    repeat(t) {
+        //TODO: Read in each Test Case
+
+        VanyaAndExams()
+            .solveTestCase()
+    }
+
+    out.flush()
+}
