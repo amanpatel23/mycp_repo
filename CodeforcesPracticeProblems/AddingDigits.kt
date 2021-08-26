31efe45cfeb62d14b8fbe2ef0c58ff576e19abef
AMAN PATEL <ap289503@gmail.com>
2021-08-27 00:16:42 +0530

    added AddingDigits into Practice folder

---
+++ CodeforcesPracticeProblems/AddingDigits.kt
@@ -0,0 +1,86 @@
+/* 
+    Author: Aman Patel
+    Date: 27-08-2021
+*/
+
+import java.io.PrintWriter
+import java.util.StringTokenizer
+import java.io.File
+import java.lang.StringBuilder
+
+private val checkOnlineJudge = System.getProperty("ONLINE_JUDGE") == null
+private val INPUT = if (checkOnlineJudge) File("src/input.txt").inputStream() else System.`in`
+private val OUTPUT = if (checkOnlineJudge) File("src/output.txt").outputStream() else System.out
+
+private val bufferedReader = INPUT.bufferedReader()
+private val out = PrintWriter(OUTPUT, false)
+private fun readLn() = bufferedReader.readLine()!!
+
+private fun readList() = readLn().split(' ')
+private var tokenizer = StringTokenizer("")
+private fun read(): String {
+    while (tokenizer.hasMoreTokens().not()) tokenizer = StringTokenizer(readLn(), " ")
+    return tokenizer.nextToken()
+}
+
+private fun readInt() = read().toInt()
+private fun readLong() = read().toLong()
+private fun readDouble() = read().toDouble()
+
+private fun readIntList() = readList().map { it.toInt() }
+private fun readLongList() = readList().map { it.toLong() }
+private fun readDoubleList() = readList().map { it.toDouble() }
+
+private fun readIntArray(n: Int = 0) =
+    if (n == 0) readList().run { IntArray(size) { get(it).toInt() } } else IntArray(n) { readInt() }
+
+private fun readLongArray(n: Int = 0) =
+    if (n == 0) readList().run { LongArray(size) { get(it).toLong() } } else LongArray(n) { readLong() }
+
+private fun readDoubleArray(n: Int = 0) =
+    if (n == 0) readList().run { DoubleArray(size) { get(it).toDouble() } } else DoubleArray(n) { readDouble() }
+
+private class AddingDigits {
+    fun solveTestCase() {
+        //TODO: Solve the question
+
+        var (a, b, n) = readIntList()
+
+        var flag = false
+        for (i in 0..9) {
+            if (((a * 10) + i) % b == 0) {
+                a = (a * 10) + i
+                n--
+                flag = true
+                break
+            }
+        }
+
+        if (flag) {
+            val result = StringBuilder()
+            result.append(a)
+            while (n > 0) {
+                result.append(0)
+                n--
+            }
+
+            out.println(result)
+        } else {
+            out.println("-1")
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+
+    var t = 1
+    //t = readInt()
+    repeat(t) {
+        //TODO: Read in each Test Case
+
+        AddingDigits()
+            .solveTestCase()
+    }
+
+    out.flush()
+}
