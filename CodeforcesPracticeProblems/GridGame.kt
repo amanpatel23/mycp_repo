bdcf639638073f0268b2f832f6401ebc99bd15eb
AMAN PATEL <ap289503@gmail.com>
2021-09-09 16:09:35 +0530

    added GridGame into Practice folder

---
+++ CodeforcesPracticeProblems/GridGame.kt
@@ -0,0 +1,73 @@
+/* 
+    Author: Aman Patel
+    Date: 09-09-2021
+*/
+
+import java.io.PrintWriter
+import java.util.StringTokenizer
+import java.io.File
+
+private val checkOnlineJudge = System.getProperty("ONLINE_JUDGE") == null
+private val INPUT = if (checkOnlineJudge) File("src/input.txt").inputStream() else System.`in`
+private val OUTPUT = if (checkOnlineJudge) File("src/output.txt").outputStream() else System.out
+
+private val bufferedReader = INPUT.bufferedReader()
+private val out = PrintWriter(OUTPUT, false)
+private fun readLn() = bufferedReader.readLine()!!
+
+private fun readList() = readLn().split(' ')
+private var tokenizer = StringTokenizer("")
+private fun read(): String {
+    while (tokenizer.hasMoreTokens().not()) tokenizer = StringTokenizer(readLn(), " ")
+    return tokenizer.nextToken()
+}
+
+private fun readInt() = read().toInt()
+private fun readLong() = read().toLong()
+private fun readDouble() = read().toDouble()
+
+private fun readIntList() = readList().map { it.toInt() }
+private fun readLongList() = readList().map { it.toLong() }
+private fun readDoubleList() = readList().map { it.toDouble() }
+
+private fun readIntArray(n: Int = 0) =
+    if (n == 0) readList().run { IntArray(size) { get(it).toInt() } } else IntArray(n) { readInt() }
+
+private fun readLongArray(n: Int = 0) =
+    if (n == 0) readList().run { LongArray(size) { get(it).toLong() } } else LongArray(n) { readLong() }
+
+private fun readDoubleArray(n: Int = 0) =
+    if (n == 0) readList().run { DoubleArray(size) { get(it).toDouble() } } else DoubleArray(n) { readDouble() }
+
+private class GridGame {
+    fun solveTestCase() {
+        //TODO: Solve the question
+
+        val str = readLn()
+
+        var (c1, c2) = listOf(0, 0)
+        for (char in str) {
+            if (char == '0') {
+                out.println("1 ${c1 + 1}")
+                c1 = (c1 + 1) % 4
+            } else {
+                out.println("3 ${c2 + 1}")
+                c2 = (c2 + 2) % 4
+            }
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+
+    var t = 1
+    //t = readInt()
+    repeat(t) {
+        //TODO: Read in each Test Case
+
+        GridGame()
+            .solveTestCase()
+    }
+
+    out.flush()
+}
