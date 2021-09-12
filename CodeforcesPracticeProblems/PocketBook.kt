f65b5e129d7bd374152497240eca022dbee3d1ef
AMAN PATEL <ap289503@gmail.com>
2021-09-12 12:42:08 +0530

    added PocketBook into Practice folder

---
+++ CodeforcesPracticeProblems/PocketBook.kt
@@ -0,0 +1,74 @@
+/* 
+    Author: Aman Patel
+    Date: 12-09-2021
+*/
+
+import java.io.PrintWriter
+import java.util.StringTokenizer
+import java.io.File
+
+private val checkOnlineJudge = System.getProperty("ONLINE_JUDGE") == null
+private val INPUT = if (checkOnlineJudge) File("src/input.txt").inputStream() else System.`in`
+private val OUTPUT = if (checkOnlineJudge) File("src/output.txt").outputStream() else System.out
+
+private val bufferedReader = INPUT.bufferedReader()
+private val out = PrintWriter(OUTPUT, false)
+private fun readLn() = bufferedReader.readLine()!!
+
+private fun readList() = readLn().split(' ')
+private var tokenizer = StringTokenizer("")
+private fun read(): String {
+    while (tokenizer.hasMoreTokens().not()) tokenizer = StringTokenizer(readLn(), " ")
+    return tokenizer.nextToken()
+}
+
+private fun readInt() = read().toInt()
+private fun readLong() = read().toLong()
+private fun readDouble() = read().toDouble()
+
+private fun readIntList() = readList().map { it.toInt() }
+private fun readLongList() = readList().map { it.toLong() }
+private fun readDoubleList() = readList().map { it.toDouble() }
+
+private fun readIntArray(n: Int = 0) =
+    if (n == 0) readList().run { IntArray(size) { get(it).toInt() } } else IntArray(n) { readInt() }
+
+private fun readLongArray(n: Int = 0) =
+    if (n == 0) readList().run { LongArray(size) { get(it).toLong() } } else LongArray(n) { readLong() }
+
+private fun readDoubleArray(n: Int = 0) =
+    if (n == 0) readList().run { DoubleArray(size) { get(it).toDouble() } } else DoubleArray(n) { readDouble() }
+
+private const val mod = 1e9.toInt() + 7
+
+private class PocketBook {
+    fun solveTestCase() {
+        //TODO: Solve the question
+
+        val (n, m) = readIntList()
+        val strings = Array(n) { readLn() }
+
+        var result = 1L
+        for (i in 0 until m) {
+            val charSet = mutableSetOf<Char>()
+            strings.forEach { charSet.add(it[i]) }
+            result = (result * charSet.size) % mod
+        }
+
+        out.println(result)
+    }
+}
+
+fun main(args: Array<String>) {
+
+    var t = 1
+    //t = readInt()
+    repeat(t) {
+        //TODO: Read in each Test Case
+
+        PocketBook()
+            .solveTestCase()
+    }
+
+    out.flush()
+}
