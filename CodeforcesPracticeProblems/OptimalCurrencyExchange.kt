50264880eb371941e685809abafd91a5a517e9d3
AMAN PATEL <ap289503@gmail.com>
2021-09-26 23:34:33 +0530

    added OptimalCurrencyExchange into Practice folder

---
+++ CodeforcesPracticeProblems/OptimalCurrencyExchange.kt
@@ -0,0 +1,79 @@
+/* 
+    Author: Aman Patel
+    Date: 26-09-2021
+*/
+
+import java.io.PrintWriter
+import java.util.StringTokenizer
+import java.io.File
+
+private val checkOnlineJudge = System.getProperty("ONLINE_JUDGE") == null
+private val INPUT = if (checkOnlineJudge) File("src/input.txt").inputStream() else System.`in`
+private val OUTPUT = if (checkOnlineJudge) File("src/output.txt").outputStream() else System.out
+
+private val bufferedReader = INPUT.bufferedReader()
+private val out = PrintWriter(OUTPUT, false)
+private fun readLn() = bufferedReader.readLine()!!
+
+private fun readList() = readLn().split(' ')
+private var tokenizer = StringTokenizer("")
+private fun read(): String {
+    while (tokenizer.hasMoreTokens().not()) tokenizer = StringTokenizer(readLn(), " ")
+    return tokenizer.nextToken()
+}
+
+private fun readInt() = read().toInt()
+private fun readLong() = read().toLong()
+private fun readDouble() = read().toDouble()
+
+private fun readIntList() = readList().map { it.toInt() }
+private fun readLongList() = readList().map { it.toLong() }
+private fun readDoubleList() = readList().map { it.toDouble() }
+
+private fun readIntArray(n: Int = 0) =
+    if (n == 0) readList().run { IntArray(size) { get(it).toInt() } } else IntArray(n) { readInt() }
+
+private fun readLongArray(n: Int = 0) =
+    if (n == 0) readList().run { LongArray(size) { get(it).toLong() } } else LongArray(n) { readLong() }
+
+private fun readDoubleArray(n: Int = 0) =
+    if (n == 0) readList().run { DoubleArray(size) { get(it).toDouble() } } else DoubleArray(n) { readDouble() }
+
+private const val iMax = (1e9).toInt()
+
+private class OptimalCurrencyExchange {
+    fun solveTestCase() {
+        //TODO: Solve the question
+
+        val n = readInt()
+        val d = readInt()
+        val e = readInt() * 5
+
+        var oneDollar = n / d
+        var exchange = 0
+        while (oneDollar >= 0) {
+            val fiveEuro = (n - (oneDollar * d)) / e
+            val currExchanged = (oneDollar * d) + (fiveEuro * e)
+            exchange = maxOf(exchange, currExchanged)
+
+            oneDollar--
+        }
+
+        val result = n - exchange
+        out.println(result)
+    }
+}
+
+fun main(args: Array<String>) {
+
+    var t = 1
+    //t = readInt()
+    repeat(t) {
+        //TODO: Read in each Test Case
+
+        OptimalCurrencyExchange()
+            .solveTestCase()
+    }
+
+    out.flush()
+}
