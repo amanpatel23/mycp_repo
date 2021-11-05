8378df0c4f632343938fa59990d68afbce57804e
AMAN PATEL <ap289503@gmail.com>
2021-11-05 19:08:59 +0530

    added Tetrahedron into Practice folder

---
+++ CodeforcesPracticeProblems/Tetrahedron.java
@@ -0,0 +1,105 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Tetrahedron {
+
+    static FastScanner fs;
+    static FastWriter fw;
+    static boolean checkOnlineJudge = System.getProperty("ONLINE_JUDGE") == null;
+
+    private static final int mod = (int) (1e9 + 7);
+
+    public static void main(String[] args) throws IOException {
+
+        fs = new FastScanner();
+        fw = new FastWriter();
+
+        int t = 1;
+        //t = fs.nextInt();
+        while (t-- > 0) {
+            solve();
+        }
+
+        fw.out.close();
+    }
+
+    static void solve() {
+
+        int n = fs.nextInt();
+        long[] arr = new long[4];
+        Arrays.fill(arr, 0);
+        arr[3] = 1;
+        long sum = 1;
+        for (int i = 1; i <= n; i++) {
+            arr[0] = (sum - arr[0] + mod) % mod;
+            arr[1] = (sum - arr[1] + mod) % mod;
+            arr[2] = (sum - arr[2] + mod) % mod;
+            arr[3] = (sum - arr[3] + mod) % mod;
+
+            sum = ((arr[0] + arr[1]) % mod + (arr[2] + arr[3]) % mod) % mod;
+        }
+
+        fw.out.println(arr[3]);
+    }
+
+    private static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner() throws IOException {
+            if (checkOnlineJudge)
+                this.br = new BufferedReader(new FileReader("src/input.txt"));
+            else
+                this.br = new BufferedReader(new InputStreamReader(System.in));
+
+            this.st = new StringTokenizer("");
+        }
+
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException err) {
+                    err.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public String nextLine() {
+            if (st.hasMoreTokens()) {
+                return st.nextToken("").trim();
+            }
+            try {
+                return br.readLine().trim();
+            } catch (IOException err) {
+                err.printStackTrace();
+            }
+            return "";
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+
+    private static class FastWriter {
+        PrintWriter out;
+
+        FastWriter() throws IOException {
+            if (checkOnlineJudge)
+                out = new PrintWriter(new FileWriter("src/output.txt"));
+            else
+                out = new PrintWriter(System.out);
+        }
+    }
+}
