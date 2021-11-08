e5466fb7f7cec7c1933b87ba149e1b24ee10c1c0
AMAN PATEL <ap289503@gmail.com>
2021-11-09 01:45:52 +0530

    added ViewsMatter into Practice folder

---
+++ CodeforcesPracticeProblems/ViewsMatter.java
@@ -0,0 +1,105 @@
+import java.io.*;
+import java.util.*;
+
+public class ViewsMatter {
+
+    static FastScanner fs;
+    static FastWriter fw;
+    static boolean checkOnlineJudge = System.getProperty("ONLINE_JUDGE") == null;
+
+    private static final int mod = (int) (1e9 + 7);
+
+    public static void main(String[] args) throws IOException {
+
+        fs = new FastScanner();
+        fw = new FastWriter();
+
+        int t = 1;
+        //t = fs.nextInt();
+        while (t-- > 0) {
+            solve();
+        }
+
+        fw.out.close();
+    }
+
+    static void solve() {
+
+        int n = fs.nextInt(), m = fs.nextInt();
+        List<Integer> _sizes = new ArrayList<>();
+        _sizes.add(0);
+        for (int i = 0; i < n; i++)
+            _sizes.add(fs.nextInt());
+        _sizes.sort(Collections.reverseOrder());
+
+        int level = _sizes.get(0);
+        long global = 0;
+        for (int i = 0; i < n; i++) {
+            int _max = Math.max(level - _sizes.get(i + 1), 1);
+            global += (_sizes.get(i) - _max);
+            level = Math.max(level - _max, 1);
+        }
+
+        fw.out.println(global);
+    }
+
+    private static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner() throws IOException {
+            if (checkOnlineJudge)
+                this.br = new BufferedReader(new FileReader("src/input.txt"));
+            else
+                this.br = new BufferedReader(new InputStreamReader(System.in));
+
+            this.st = new StringTokenizer("");
+        }
+
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException err) {
+                    err.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public String nextLine() {
+            if (st.hasMoreTokens()) {
+                return st.nextToken("").trim();
+            }
+            try {
+                return br.readLine().trim();
+            } catch (IOException err) {
+                err.printStackTrace();
+            }
+            return "";
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+
+    private static class FastWriter {
+        PrintWriter out;
+
+        FastWriter() throws IOException {
+            if (checkOnlineJudge)
+                out = new PrintWriter(new FileWriter("src/output.txt"));
+            else
+                out = new PrintWriter(System.out);
+        }
+    }
+}
