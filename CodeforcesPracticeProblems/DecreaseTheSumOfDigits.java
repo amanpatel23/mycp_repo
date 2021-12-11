b8511bbe6fbbd9f7f60314121c86ffdd5769b283
AMAN PATEL <ap289503@gmail.com>
2021-12-11 12:56:29 +0530

    added DecreaseSumOfDigits into Practice folder

---
+++ CodeforcesPracticeProblems/DecreaseTheSumOfDigits.java
@@ -0,0 +1,123 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class DecreaseTheSumOfDigits {
+
+    static FastScanner fs;
+    static FastWriter fw;
+    static boolean checkOnlineJudge = System.getProperty("ONLINE_JUDGE") == null;
+
+    private static final int mod = (int) (1e9 + 7);
+
+    public static void main(String[] args) throws IOException {
+
+        fs = new FastScanner();
+        fw = new FastWriter();
+
+        int t = 1;
+        t = fs.nextInt();
+        while (t-- > 0) {
+            solve();
+        }
+
+        fw.out.close();
+    }
+
+    static void solve() {
+
+        long num = fs.nextLong();
+        int sum = fs.nextInt();
+
+        long modified_num = num;
+        while (true) {
+            int curr_sum = sumOfDigits(modified_num);
+            if (curr_sum <= sum) {
+                fw.out.println((modified_num - num));
+                return;
+            }
+            modified_num += increaseBy(modified_num);
+        }
+    }
+
+    private static int sumOfDigits(long num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += (num % 10);
+            num /= 10;
+        }
+        return sum;
+    }
+
+    private static long increaseBy(long num) {
+
+        long appendZeroes = 1;
+        while (true) {
+            int curr = (int) (num % 10);
+            if (curr > 0) {
+                return ((10 - curr) * appendZeroes);
+            }
+            appendZeroes *= 10;
+            num /= 10;
+        }
+    }
+
+    private static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner() throws IOException {
+            if (checkOnlineJudge)
+                this.br = new BufferedReader(new FileReader("src/input.txt"));
+            else
+                this.br = new BufferedReader(new InputStreamReader(System.in));
+
+            this.st = new StringTokenizer("");
+        }
+
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException err) {
+                    err.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public String nextLine() {
+            if (st.hasMoreTokens()) {
+                return st.nextToken("").trim();
+            }
+            try {
+                return br.readLine().trim();
+            } catch (IOException err) {
+                err.printStackTrace();
+            }
+            return "";
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+
+    private static class FastWriter {
+        PrintWriter out;
+
+        FastWriter() throws IOException {
+            if (checkOnlineJudge)
+                out = new PrintWriter(new FileWriter("src/output.txt"));
+            else
+                out = new PrintWriter(System.out);
+        }
+    }
+}
